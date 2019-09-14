7244889ad1e9ca41a72dba5cba01e3e870452ad7
Daryl Magera <darylmagera@gmail.com>
2019-09-14 15:29:46 -0400

    added a few styles

--- src/js/config.js
+++ src/js/config.js
@@ -1,4 +1,4 @@
-export const key = '8d395b77b9ce5645626ccae8d7fb1b7d';
+export const key = '983743d2387e93bccfa2abaa4fe75bd3';
 
 //5: c9b2a792e6067e39fa8f231b68829c65
 //4: 84e724cd723256b4e9bd8302bd60be38
